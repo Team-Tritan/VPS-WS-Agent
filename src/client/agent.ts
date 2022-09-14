28e68fda0d8c5c9e3f225120e1215d0b399430ab
Dylan J <dylan@suhd00d.co>
2022-09-14 02:54:36 -0400

    Update agent.ts

--- src/client/agent.ts
+++ src/client/agent.ts
@@ -1,3 +1,5 @@
 import { initWsConnection } from "./ws/initConnection";
+
 console.log(`[WS Client] --> Started, will attempt connect in 1 minute.`);
+
 setInterval(() => initWsConnection(), 1 * 60000);
