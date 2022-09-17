52a2cdc5783ae8e59a04d2fdeeea85e269feefc4
Dylan J <dylan@suhd00d.co>
2022-09-16 21:30:00 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export default {
   hostname: "code.colo-fl.myinfra.lol",
-  ws_port: 8080,
+  ws_port: 80,
   mongodb: "mongodb://tritan.colo-fl.myinfra.lol:27017/VM-Healthchecks",
   agent_update_ms: 1 * 60000, // 10 min
   agent_reconnect_delay: 1 * 10000, // 10 seconds
