4a9adc7e1bec2948e705430fcefdcf1335253d70
Sexy Developer <johndoe@example.com>
2022-09-18 06:42:24 +0100

    prod hostname

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
-  hostname: "code.colo-fl.myinfra.lol",
-  ws_port: 8080,
+  hostname: "ws.colo-fl.myinfra.lol",
+  ws_port: 80,
   mongodb: "mongodb://tritan.colo-fl.myinfra.lol:27017/VM-Healthchecks",
   agent_update_ms: 10 * 60000, // 10 min
   agent_reconnect_delay: 1 * 10000, // 10 seconds
