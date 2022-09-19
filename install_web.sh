ae29b165739af1d114b1044ddf987cdfd938e238
Sexy Developer <johndoe@example.com>
2022-09-19 23:04:15 +0100

    web install script

---
+++ install_web.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get upgrade -y
+sudo apt install docker docker-compose -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo docker-compose up -d
+sudo docker-compose kill agent
+sudo docker-compose kill server
+
