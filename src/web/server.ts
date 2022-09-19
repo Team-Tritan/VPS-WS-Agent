605b4449d264b492ba9af5fd7ba631268ba28c0d
Sexy Developer <johndoe@example.com>
2022-09-19 23:34:41 +0100

    convert web to class based to match rest of proj

--- src/web/server.ts
+++ src/web/server.ts
@@ -3,27 +3,42 @@ import { initDatabase } from "./functions/database";
 import dataModel from "./models/dataModel";
 import config from "../config";
 
-let app: Application = express();
+class WebHelper {
+  app: Application = express();
 
-initDatabase();
+  constructor() {
+    this.setup();
+    this.uiRoute();
+    this.listen();
+  }
 
-app.set("view engine", "ejs");
-app.use(express.json());
+  setup() {
+    initDatabase();
+    this.app.set("view engine", "ejs");
+    this.app.use(express.json());
+  }
 
-app.get("/", async (_req: Request, res: Response) => {
-  let dbData = await dataModel.find();
-  let arr: Array<Object> = [];
+  uiRoute() {
+    this.app.get("/", async (_req: Request, res: Response) => {
+      let dbData = await dataModel.find();
+      let arr: Array<Object> = [];
 
-  await dbData.forEach((x) => {
-    arr.push({ hostname: x.hostname, data: x.data });
-  });
+      await dbData.forEach((x) => {
+        arr.push({ hostname: x.hostname, data: x.data });
+      });
 
-  return res.render("index", {
-    hosts: arr,
-    update_time: config.agent_update_ms,
-  });
-});
+      return res.render("index", {
+        hosts: arr,
+        update_time: config.agent_update_ms,
+      });
+    });
+  }
 
-app.listen(config.web_port, () => {
-  console.log(`[WS Web Helper] --> Listening on port ${config.web_port}.`);
-});
+  listen() {
+    this.app.listen(config.web_port, () => {
+      console.log(`[WS Web Helper] --> Listening on port ${config.web_port}.`);
+    });
+  }
+}
+
+new WebHelper();
