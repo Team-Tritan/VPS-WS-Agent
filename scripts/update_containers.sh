4273bba6b170aee83bf3096059e1a5801dea6045
Sexy Developer <johndoe@example.com>
2022-09-19 23:49:13 +0100

    update script

---
+++ scripts/update_containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd  ..
+docker-compose kill 
+docker system prune -a 
+git pull
+docker-compose up -d
+echo "Started ALL services, make sure to kill the ones you don't need on this node. "
