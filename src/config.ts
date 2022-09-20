9399427acef6746687b8afb5337c3f17d063d0c3
Sexy Developer <johndoe@example.com>
2022-09-20 01:42:01 +0100

    changing agent time

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export default {
   ws_server_port: 80,
   web_helper_port: 8080,
   mongodb_uri: "mongodb://tritan.colo-fl.myinfra.lol:27017/VM-Healthchecks",
-  agent_update_ms: 10 * 60000, // 10 min
+  agent_update_ms: 1 * 60000, // 10 min
   agent_reconnect_delay: 1 * 10000, // 10 seconds
 };
